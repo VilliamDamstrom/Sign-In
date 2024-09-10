3a81a30014c773c2a14b86b5cc0a9835d90e3c89
villiam damstrom <villiam@live.se>
2024-09-10 18:44:36 +0200

    Added html and javascript
    Added some js code and styled the main in the js code. will style the rest in css

---
+++ script.js
@@ -0,0 +1,25 @@
+const correctUsername = "test";
+const correctPassword = "test";
+
+function renderLoginPage() {
+    const root = document.getElementById('root');
+
+}
+
+
+// Styled it here but prefer to style in css, makes the script file look cleaner imo
+let main = document.createElement("main");
+main.style.width = "600px";
+main.style.minHeight = "400px";
+main.style.backgroundColor = "darkblue";
+main.style.display = "flex";
+main.style.justifyContent = "center";
+main.style.alignItems = "center";
+main.style.margin = "0 auto";
+main.style.marginTop ="20%";
+main.style.borderRadius ="25px";
+main.style.padding ="20px";
+
+
+root.appendChild(main);
+
