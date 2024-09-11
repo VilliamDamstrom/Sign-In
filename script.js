b1db9e54ea0fdb524f29d5e4edfb082eb73d9523
villiam damstrom <villiam@live.se>
2024-09-11 14:53:49 +0200

    Updated .js and .css
    Updated .js, added function for login  and if else alert for both options, Also some css

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const correctPassword = "1234";
 function renderLoginPage() {
     const root = document.getElementById('root');
 
+    root.innerHTML = '';
+
 // Styled it here but prefer to style in css, makes the script file look cleaner imo
 let main = document.createElement("main");
 main.style.width = "600px";
@@ -13,12 +15,12 @@ main.style.display = "flex";
 main.style.justifyContent = "center";
 main.style.alignItems = "center";
 main.style.margin = "0 auto";
-main.style.marginTop ="20%";
+main.style.marginTop ="150px";
 main.style.borderRadius ="25px";
 main.style.padding ="20px";
 
 let container = document.createElement("div");
-container.loginForm.add("login-form");
+container.classList.add("login-form");
 
 const header = document.createElement("h2");
 header.textContent = "Logga in";
@@ -37,7 +39,7 @@ loginBtn.addEventListener('click', login);
 
 // Add elements to container
 container.appendChild(header);
-continuer.appendChild(usernameInput);
+container.appendChild(usernameInput);
 container.appendChild(passwordInput);
 container.appendChild(loginBtn);
 
@@ -49,8 +51,15 @@ root.appendChild(main);
 
 }
 
+function login(username, password) {
+    if (username === correctUsername && password === correctPassword) {
+        alert("Välkommen!");
+    } else {
+        alert("Fel användarnamn eller lösenord!");
+    }
+}
 
-
+renderLoginPage();
 
 
 
