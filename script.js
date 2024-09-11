299db01f0de8712c0bb034558e7f0b7396224539
villiam damstrom <villiam@live.se>
2024-09-11 14:27:09 +0200

    Updated more .js
    Added a log in button and added the elements to the containers using appendChild.

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const correctPassword = "1234";
 function renderLoginPage() {
     const root = document.getElementById('root');
 
-}
-
-
 // Styled it here but prefer to style in css, makes the script file look cleaner imo
 let main = document.createElement("main");
 main.style.width = "600px";
@@ -20,20 +17,43 @@ main.style.marginTop ="20%";
 main.style.borderRadius ="25px";
 main.style.padding ="20px";
 
-let.container = document.createElement("div");
+let container = document.createElement("div");
 container.loginForm.add("login-form");
 
-const heading = document.createElement("h2");
-heading.textContent = "Logga in";
+const header = document.createElement("h2");
+header.textContent = "Logga in";
 
-const usernameInput = document.createElement("input");
+const usernameInput = document.createElement('input');
 usernameInput.type = "text";
 usernameInput.placeholder = "Användarnamn"
 
-const passwordInput = document.createElement("input");
+const passwordInput = document.createElement('input');
 passwordInput.type = "password";
 passwordInput.placeholder = "Lösenord";
 
+const loginBtn = document.createElement('button');
+loginBtn.textContent = 'Login';
+loginBtn.addEventListener('click', login);
 
+// Add elements to container
+container.appendChild(header);
+continuer.appendChild(usernameInput);
+container.appendChild(passwordInput);
+container.appendChild(loginBtn);
+
+// App container to main
+main.appendChild(container);
+
+// Append main to the root
 root.appendChild(main);
+
+}
+
+
+
+
+
+
+
+
 
