89c5e3fe410e06b5ca4bd90a7fe1b036c09253f7
villiam damstrom <villiam@live.se>
2024-09-11 15:44:55 +0200

    Update
    Updated html, css and js so now there is also a welcome page if you log in correctly.

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const correctPassword = "1234";
 
 function renderLoginPage() {
     const root = document.getElementById('root');
-
+    // Clear root content
     root.innerHTML = '';
 
-// Styled it here but prefer to style in css, makes the script file look cleaner imo
+// Styled the main here but prefer to style in css,
+// makes the script file look cleaner imo
 let main = document.createElement("main");
 main.style.width = "600px";
 main.style.minHeight = "400px";
@@ -34,8 +35,10 @@ passwordInput.type = "password";
 passwordInput.placeholder = "Lösenord";
 
 const loginBtn = document.createElement('button');
-loginBtn.textContent = 'Login';
-loginBtn.addEventListener('click', login);
+loginBtn.textContent = 'Logga in!';
+loginBtn.addEventListener('click', function() {
+    login(usernameInput.value, passwordInput.value);
+});
 
 // Add elements to container
 container.appendChild(header);
@@ -45,15 +48,58 @@ container.appendChild(loginBtn);
 
 // App container to main
 main.appendChild(container);
-
 // Append main to the root
 root.appendChild(main);
 
 }
 
+
+function renderWelcomePage() {
+    const root = document.getElementById('root');
+    // Clear root content
+    root.innerHTML = '';
+
+// styled the main on the welcome page here aswell since it was almost
+// the same code    
+let main = document.createElement("main");
+main.style.width = "600px";
+main.style.minHeight = "400px";
+main.style.backgroundColor = "darkred";
+main.style.display = "flex";
+main.style.justifyContent = "center";
+main.style.alignItems = "center";
+main.style.margin = "0 auto";
+main.style.marginTop ="150px";
+main.style.borderRadius ="25px";
+main.style.padding ="20px";
+
+let container = document.createElement("div");
+container.classList.add("welcome-form");
+
+const welcomeHeader = document.createElement("h2");
+welcomeHeader.textContent = "Välkommen!"
+
+ const logoutBtn = document.createElement('button');
+logoutBtn.textContent = "Logga ut!";
+ logoutBtn.addEventListener('click', function() {
+    logoutBtn();
+});
+
+container.appendChild(welcomeHeader);
+container.appendChild(logoutBtn);
+
+// Add container to main
+main.appendChild(container);
+// Append main to the root
+ root.appendChild(main);
+
+}
+
+
 function login(username, password) {
     if (username === correctUsername && password === correctPassword) {
-        alert("Välkommen!");
+        localStorage.setItem('loggedIn', 'true');
+
     } else {
         alert("Fel användarnamn eller lösenord!");
     }
